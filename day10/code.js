c3c68d0b3aaeef976845984d5fafb5da1230431b
Shaw Tan <shawtan@meta.com>
2023-12-09 23:23:25 -0800

    day 10 pt1

--- day10/code.js
+++ day10/code.js
@@ -1,45 +1,76 @@
 const fs = require('node:fs');
 
-const input = fs.readFileSync('example.txt', 'utf16le');
-// const input = fs.readFileSync('input.txt', 'utf16le');
+// const input = fs.readFileSync('example.txt', 'utf8');
+const input = fs.readFileSync('input.txt', 'utf8');
 
 const data = input.split('\n').map(x => x.split('').filter(y => y != '\r' && y != ' '));
 
 const findS = () => {
-    for (let i in data) {
-        for (let j in data[i]) {
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length; j++) {
             if (data[i][j] === "S") {
-                console.log('S at ', i, j);
+                // console.log('S at ', i, j);
                 return [i, j];
             }
         }
     }
 }
 const S = findS();
+const MAX = 20000;
 
 const dp = [];
 for (let i in data) {
     dp[i] = [];
+    for (let j in data[i]) {
+        dp[i][j] = MAX;
+    }
 }
 
-const MAX = 20000;
+
+// north east south west
+const NORTH = 0;
+const EAST = 1;
+const SOUTH = 2;
+const WEST = 3;
+const SYMBOLS = {
+    "|": [true, false, true, false],
+    "-": [false, true, false, true],
+    "L": [true, true, false, false],
+    "J": [true, false, false, true],
+    "7": [false, false, true, true],
+    "F": [false, true, true, false],
+    ".": [false, false, false, false],
+    "S": [true, true, true, true],
+};
 
 let max = 0;
 
 const checkAdjacent = (i, j) => {
     if (i < 0 || i >= data.length) {
-        return [];
+        return [MAX, []];
     }
     if (j < 0 || j >= data[0].length) {
-        return [];
+        return [MAX, []];
     }
     let val = dp[i][j];
     let coord = [i, j];
     if (val < MAX) {
         return [val, []];
     } else {
-        return [val, coord];
+        return [val, [coord]];
     }
+}
+
+const isSquareValid = (i, j, direction) => {
+    if (i < 0 || i >= data.length) {
+        return false;
+    }
+    if (j < 0 || j >= data[0].length) {
+        return false;
+    }
+    const char = data[i][j];
+    // console.log('isSquareValid', char, SYMBOLS[char]);
+    return SYMBOLS[char][direction];
 }
 const parseSymbol = (i, j) => {
     if (i < 0 || i >= data.length) {
@@ -48,89 +79,199 @@ const parseSymbol = (i, j) => {
     if (j < 0 || j >= data[0].length) {
         return [];
     }
-    if (dp[i][j] !== undefined) {
+    if (dp[i][j] < MAX) {
+        // console.log('skip', i, j);
         return [];
     }
     const char = data[i][j];
-    const check = [];
-    let a, b, coord, val;
-    switch (char) {
-        case "S":
-            dp[i][j] = 0;
-            let offsets = [
-                [-1, 0],
-                [0, -1],
-                [0, 1],
-                [1, 0]
-            ];
-            for (let [x, y] of offsets) {
+    const charDirections = SYMBOLS[char];
+    let val = MAX;
+    const check = []
+
+    if (char === "S") {
+        dp[i][j] = 0;
+        let offsets = [
+            [-1, 0, SOUTH],
+            [0, -1, EAST],
+            [0, 1, WEST],
+            [1, 0, NORTH]
+        ];
+        for (let [x, y, d] of offsets) {
+            if (isSquareValid(i + x, j + y, d)) {
                 check.push([i + x, j + y]);
             }
-            return check;
-        case ".":
-            return [];
-        case "|":
-            [a, coord] = checkAdjacent(i - 1, j);
-            check = check.concat(coord);
-            [b, coord] = checkAdjacent(i + 1, j);
-            check = check.concat(coord);
-            val = Math.min(a, b) + 1;
-            break;
-        case "-":
-            [a, coord] = checkAdjacent(i, j - 1);
-            check = check.concat(coord);
-            [b, coord] = checkAdjacent(i, j + 1);
-            check = check.concat(coord);
-            val = Math.min(a, b) + 1;
-            break;
-        case "L":
-            [a, coord] = checkAdjacent(i - 1, j);
-            check = check.concat(coord);
-            [b, coord] = checkAdjacent(i, j + 1);
-            check = check.concat(coord);
-            val = Math.min(a, b) + 1;
-            break;
-
-        case "7":
-            [a, coord] = checkAdjacent(i, j - 1);
-            check = check.concat(coord);
-            [b, coord] = checkAdjacent(i + 1, j);
-            check = check.concat(coord);
-            val = Math.min(a, b) + 1;
-            break;
-
-        case "F":
-            [a, coord] = checkAdjacent(i, j + 1);
-            check = check.concat(coord);
-            [b, coord] = checkAdjacent(i + 1, j);
-            check = check.concat(coord);
-            val = Math.min(a, b) + 1;
-            break;
+        }
+        return check;
     }
 
-    max = Math.max(val, max);
-    dp[i][j] = val;
+    // console.log(i, j, char, 'has directions', charDirections);
+
+    // north
+    if (charDirections[NORTH]) {
+        if (isSquareValid(i - 1, j, SOUTH)) {
+            let squareVal = dp[i - 1][j];
+            if (squareVal < MAX) {
+                val = Math.min(val, dp[i - 1][j]);
+            } else {
+                check.push([i - 1, j]);
+            }
+        }
+    }
+
+    if (charDirections[EAST]) {
+        // console.log('east square');
+        let x = i;
+        let y = j + 1;
+        if (isSquareValid(x, y, WEST)) {
+            let squareVal = dp[x][y];
+            // console.log(x, y, 'is valid', squareVal);
+            if (squareVal < MAX) {
+                val = Math.min(val, dp[x][y]);
+            } else {
+                check.push([x, y]);
+            }
+        }
+    }
+
+    if (charDirections[SOUTH]) {
+        let x = i + 1;
+        let y = j;
+        if (isSquareValid(x, y, NORTH)) {
+            let squareVal = dp[x][y];
+            if (squareVal < MAX) {
+                val = Math.min(val, dp[x][y]);
+            } else {
+                check.push([x, y]);
+            }
+        }
+    }
+
+    if (charDirections[WEST]) {
+        // console.log('west square');
+        let x = i;
+        let y = j - 1;
+        if (isSquareValid(x, y, EAST)) {
+            let squareVal = dp[x][y];
+            // console.log(x, y, 'is valid', squareVal);
+            if (squareVal < MAX) {
+                val = Math.min(val, dp[x][y]);
+            } else {
+                check.push([x, y]);
+            }
+        }
+    }
+
+    // console.log('final val for', i, j, 'is', val);
+    if (val < MAX) {
+        dp[i][j] = val + 1;
+        max = Math.max(max, dp[i][j]);
+    }
     return check;
 }
 
-console.log('data', data);
+const OUTSIDE = "O";
+
+const parseHoles = (i, j) => {
+
+    if (i < 0 || i >= data.length) {
+        return [];
+    }
+    if (j < 0 || j >= data[0].length) {
+        return [];
+    }
+    if (dp[i][j] === OUTSIDE) {
+        return [];
+    }
+    if (dp[i][j] < MAX) {
+        return [];
+    }
+
+    dp[i][j] = OUTSIDE;
+    let offsets = [
+        [-1, 0, SOUTH],
+        [0, -1, EAST],
+        [0, 1, WEST],
+        [1, 0, NORTH]
+    ];
+    let check = [];
+    for (let [x, y, d] of offsets) {
+        check.push([i + x, j + y]);
+    }
+    return check;
+}
+
+const printGrid = () => {
+    console.log("Grid:");
+    for (let i = 0; i < dp.length; i++) {
+        let row = "";
+        for (let j = 0; j < dp[0].length; j++) {
+            const n = dp[i][j];
+            if (n === "O") {
+                row += "O";
+            } else if (n === MAX) {
+                row += "#";
+            } else {
+                row += data[i][j];
+            }
+        }
+        console.log(row);
+    }
+    console.log("END Grid:");
+}
+
 const partone = () => {
     let queue = [S];
-
     while (queue.length > 0) {
         let [i, j] = queue[0];
         queue = queue.slice(1);
         const newChecks = parseSymbol(i, j);
-        console.log(`Checked ${i},${j} = ${data[i][j]} and will need to check ${newChecks}`);
         queue = queue.concat(newChecks);
     }
+    // console.log(dp);
+    // printGrid();
 
-    console.log(dp);
+    console.log('max', max);
     return max;
 }
 
 
+const parttwo = () => {
+    partone(); // calc dp
 
-const parttwo = () => {}
+    let queue = [];
 
-console.log(partone());
+    for (let i = 0; i < data.length; i++) {
+        queue.push([i, 0]);
+        queue.push([i, data[0].length - 1]);
+    }
+    for (let i = 0; i < data[0].length; i++) {
+        queue.push([0, i]);
+        queue.push([data.length - 1, i]);
+    }
+
+    while (queue.length > 0) {
+        let [i, j] = queue[0];
+
+        // if (i == data.length - 1) {
+        //     console.log('Parsing', i, j, data[i][j]);
+        // }
+        queue = queue.slice(1);
+        const newChecks = parseHoles(i, j);
+        queue = queue.concat(newChecks);
+    }
+    printGrid();
+    // console.log(dp);
+    let sum = 0;
+    for (let row of dp) {
+        for (let n of row) {
+            if (n === MAX) {
+                sum++;
+            }
+        }
+    }
+    return sum;
+
+}
+
+// console.log(partone());
+console.log(parttwo());
